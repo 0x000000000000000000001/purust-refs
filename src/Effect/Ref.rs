acd5959bd03fe05a94238acea5e4ea27ba92fbfc
0x1 <kevin.francart3@gmail.com>
2026-08-11 12:53:56 +0200

    misc

--- src/Effect/Ref.rs
+++ src/Effect/Ref.rs
@@ -1,3 +1,3 @@
-pub type Ref = crate::UnknownType;
+
 pub fn Effect_Ref__new() -> crate::UnknownType { crate::UnknownType::new(0) }
 pub fn Effect_Ref_modifyImpl() -> crate::UnknownType { crate::UnknownType::new(0) }
