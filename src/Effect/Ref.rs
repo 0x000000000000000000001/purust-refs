11298cff2a840a6da76155cfcf21a88643e5094b
0x1 <kevin.francart3@gmail.com>
2026-08-15 19:05:34 +0200

    misc

--- src/Effect/Ref.rs
+++
@@ -1,3 +0,0 @@
-
-pub fn Effect_Ref__new() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Effect_Ref_modifyImpl() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
